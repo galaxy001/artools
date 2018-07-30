31200d7765c1ff3b759cbcb3a83637a04ce8e0ea
artoo <artoo@artixlinux.org>
2018-07-30 02:21:53 +0200

    util-pkg-gitea: get compliant repo name

--- lib/util-pkg-gitea.sh
+++ lib/util-pkg-gitea.sh
@@ -1,13 +1,24 @@
  #!/bin/bash
 
+get_compliant_name(){
+    local gitname="$1"
+    case $gitname in
+        *+) gitname=${gitname//+/plus}
+    esac
+    echo $gitname
+}
+
+
 create_repo(){
     local pkg="$1"
-    curl -X POST "${git_url}/api/v1/org/packages/repos?access_token=${git_token}" -H "accept: application/json" -H "content-type: application/json" -d "{\"name\":\"$pkg\"}"
+    local gitname=$(get_compliant_name "$pkg")
+    curl -X POST "${git_url}/api/v1/org/packages/repos?access_token=${git_token}" -H "accept: application/json" -H "content-type: application/json" -d "{\"name\":\"$gitname\"}"
 }
 
 delete_repo(){
     local pkg="$1"
-    curl -X DELETE "${git_url}/api/v1/repos/packages/$pkg?access_token=${git_token}" -H  "accept: application/json"
+    local gitname=$(get_compliant_name "$pkg")
+    curl -X DELETE "${git_url}/api/v1/repos/packages/$gitname?access_token=${git_token}" -H  "accept: application/json"
 }
 
 add_repo_to_team(){
