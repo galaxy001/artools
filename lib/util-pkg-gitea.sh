37b02c08bc86df37e986a1c1c908ce28b9ce81ae
artoo <artoo@artixlinux.org>
2018-07-30 02:43:29 +0200

    util-pkg-gitea: add licence header

--- lib/util-pkg-gitea.sh
+++ lib/util-pkg-gitea.sh
@@ -1,4 +1,13 @@
- #!/bin/bash
+#!/bin/bash
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
 get_compliant_name(){
     local gitname="$1"
