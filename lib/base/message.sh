1905aef7d775ca9bb0b6e682066ece33050b7983
artoo <artoo@artixlinux.org>
2019-01-24 13:39:17 +0100

    devtools patches

--- lib/base/message.sh
+++ lib/base/message.sh
@@ -64,8 +64,30 @@ slock() {
     fi
 }
 
+_setup_workdir=false
+setup_workdir() {
+    [[ -z ${WORKDIR:-} ]] && WORKDIR=$(mktemp -d --tmpdir "${0##*/}.XXXXXXXXXX")
+    _setup_workdir=true
+    trap 'trap_abort' INT QUIT TERM HUP
+    trap 'trap_exit' EXIT
+}
+
+trap_abort() {
+    trap - EXIT INT QUIT TERM HUP
+    abort
+}
+
+trap_exit() {
+    local r=$?
+    trap - EXIT INT QUIT TERM HUP
+    cleanup $r
+}
+
 cleanup() {
-    exit ${1:-0}
+    if [[ -n ${WORKDIR:-} ]] && $_setup_workdir; then
+        rm -rf "$WORKDIR"
+    fi
+    exit "${1:-0}"
 }
 
 abort() {
