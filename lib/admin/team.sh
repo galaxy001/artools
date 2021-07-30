a8a20800cfdaedf308010aa0636fcbb68ac3100c
artoo <artoo@artixlinux.org>
2021-07-30 10:08:34 +0200

    admin: improve team checks

--- lib/admin/team.sh
+++ lib/admin/team.sh
@@ -41,11 +41,54 @@ has_team() {
     local url match
 
     url="${GIT_URL}/api/v1/repos/$org/$repo/teams/$team?access_token=${GIT_TOKEN}"
-    match=$(api_get "$url" -H  "accept: application/json"  | jq '.name')
-    if [[ $match ]]; then
+    match=$(api_get "$url" -H  "accept: application/json" | jq '.name')
+
+    msg "repo [%s] team query ..." "$repo"
+
+    if [[ "$match" != null ]]; then
+        msg2 "result: %s" "$match"
         return 0
     fi
     return 1
+}
+
+list_org_repos() {
+    local org="$1"
+    local url repos
+
+    url="${GIT_URL}/api/v1/orgs/packagesA/repos?access_token=${GIT_TOKEN}"
+
+    repos=$(curl -X GET "$url" -H  "accept: application/json" | jq '.[] .name' | tr -d \")
+
+    echo "$repos"
+}
+
+list_repo_teams() {
+    local org="$1"
+    local repo="$2"
+    local team="$3"
+    local url teams
+
+    url="${GIT_URL}/api/v1/repos/$org/$repo/teams?access_token=${GIT_TOKEN}"
+
+    msg "repo [%s] team query ..." "$repo"
+    msg2 "assigned: %s" "$team"
+    teams=($(api_get "$url" -H  "accept: application/json" | jq '.[] .name' | tr -d \"))
+
+    if (( ${#teams[@]} < 2 )); then
+        warning "repo [%s] has no team assigned! Should be (%s)" "$repo"  "$team"
+        "${add}" && add_team_to_repo "$repo" "$org" "$team"
+
+    elif (( ${#teams[@]} > 2 )); then
+        for t in "${teams[@]}"; do
+            if [[ "$t" != $team ]]; then
+                if [[ "$t" != Owners ]]; then
+                    warning "repo [%s] has wrong team (%s) assigned!" "$repo" "$t"
+                    "${remove}" && remove_team_from_repo "$repo" "$org" "$t"
+                fi
+            fi
+        done
+    fi
 }
 
 # }}}
